1d29d57b9c322214fe69e370f484d371dd0dd830
Jelena Feliciano <jfel217@gmail.com>
2021-10-10 17:39:53 -0400

    added unsplash dependency and fixed search function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import SearchPhotos  from './component/SearchPhotos';
+import SearchPhotos  from './SearchPhotos';
+
 
 function App() {
   return (
--- src/SearchPhotos.jsx
+++ src/SearchPhotos.jsx
@@ -1,11 +1,34 @@
 import React, { useState } from 'react';
+import Unsplash, { toJson } from "unsplash-js";
+
+import API_KEY from './config'
+
+const unsplash = new Unsplash({
+    accessKey: API_KEY,
+  });
+  
 
 export default function SearchPhotos() {
     const [query, setQuery] = useState("");
-    console.log(query);
+/**SearchPhotos is an asynchronous function that's trigger when the search button is clicked 
+ * unsplash.search specifies the search to photos 
+ * @params query- keyword to search for
+ * @params toJson - method to covert response to JSON 
+  */
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+        unsplash.search
+            .photos(query)
+            .then(toJson)
+            .then((json) => {
+            console.log(json);
+            });
+        
+      };
+      
     return(
         <>
-            <form className="form">
+            <form className="form" onSubmit={searchPhotos}>
                 <label className="label" htmlFor="query">
                     {" "}
                     📷
